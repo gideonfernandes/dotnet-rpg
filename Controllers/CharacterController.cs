3b675e732c4481a1a819d55437f20c85853ec55a
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-10-18 19:12:21 +0000

    Add CharacterController & Service

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,36 @@
+using dotnet_rpg.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController: ControllerBase
+    {
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character { Id = 1, Name = "Sam" }
+        };
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{Id}")]
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(characters => characters.Id == id));
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
